e534c1b639c7d3ac955ddf09d2336cc61c68ad58
jarwino <jarwino@gmail.com>
2017-07-18 22:43:56 -0400

    Updated pdflatex filename

--- piazza-to-latex.py
+++ piazza-to-latex.py
@@ -59,7 +59,7 @@ f.write("\\maketitle\n")
 f.write(text)
 f.write("\\end{document}")
 f.close()
-os.system("pdflatex piazza-export.tex")
+os.system("pdflatex piazza-export-" + class_id + ".pdf")
 if sys.platform.startswith('darwin'):
     subprocess.call(('open', "piazza-export-" + class_id + ".pdf"))
 elif os.name == 'nt':
